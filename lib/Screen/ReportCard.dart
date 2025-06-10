1736f67ef96ec3a85b5bc9c043732ae4f686ff88
Mughals Gamer <ytmusiconlyorg@gmail.com>
2025-06-10 14:46:58 +0500

    new

--- lib/Screen/ReportCard.dart
+++ lib/Screen/ReportCard.dart
@@ -4,7 +4,6 @@ import 'package:open_file/open_file.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:pdf/pdf.dart';
 import 'package:pdf/widgets.dart' as pw;
-import 'package:printing/printing.dart';
 import 'package:share_plus/share_plus.dart';
 import 'package:syncfusion_flutter_datepicker/datepicker.dart';
 import 'package:intl/intl.dart';
--- lib/main.dart
+++ lib/main.dart
@@ -1,66 +1,9 @@
-// import 'package:desicionsdiarynew/widgets/ThemeProvider.dart';
-// import 'package:firebase_auth/firebase_auth.dart';
-// import 'package:firebase_core/firebase_core.dart';
-// import 'package:flutter/cupertino.dart';
-// import 'package:flutter/material.dart';
-// import 'package:provider/provider.dart';
-//
-// import 'Screen/dashboard.dart';
-// import 'Screen/login page.dart';
-// import 'firebase_options.dart';
-//
-// Future<void> main() async {
-//   WidgetsFlutterBinding.ensureInitialized();
-//   await Firebase.initializeApp(
-//     options: DefaultFirebaseOptions.currentPlatform,
-//   );
-//
-//   runApp(
-//     ChangeNotifierProvider(
-//       create: (context) => ThemeProvider(),
-//       child: const MyApp(),
-//     ),
-//
-//   );
-// }
-//
-// class MyApp extends StatelessWidget {
-//   const MyApp({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     final themeNotifier = Provider.of<ThemeProvider>(context);
-//     return MaterialApp(
-//       debugShowCheckedModeBanner: false,
-//       themeMode: themeNotifier.isDarkMode ? ThemeMode.dark : ThemeMode.light,
-//
-//
-//       home: StreamBuilder<User?>(
-//         stream: FirebaseAuth.instance.authStateChanges(),
-//         builder: (context, snapshot) {
-//           // If the user is authenticated, show the Dashboard
-//           if (snapshot.connectionState == ConnectionState.active) {
-//             if (snapshot.hasData) {
-//               return DashboardScreen(); // Navigate to Dashboard if the user is logged in
-//             } else {
-//               return LoginPage(); // Navigate to Login if the user is not logged in
-//             }
-//           }
-//           // Loading screen while checking the authentication state
-//           return Center(child: CircularProgressIndicator());
-//         },
-//       ),
-//     );
-//   }
-// }
 
 import 'package:desicionsdiarynew/widgets/ThemeProvider.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
-
-import 'Screen/EmailVerification.dart';
 import 'Screen/dashboard.dart';
 import 'Screen/login page.dart';
 import 'firebase_options.dart';
@@ -144,8 +87,7 @@ class AuthWrapper extends StatelessWidget {
               ),
             );
           }
-          // If email is verified, go to dashboard
-          return const DashboardScreen();
+
         }
         // If no user is logged in, go to login page
         return const LoginPage();
@@ -153,23 +95,3 @@ class AuthWrapper extends StatelessWidget {
     );
   }
 }
-// class AuthWrapper extends StatelessWidget {
-//   const AuthWrapper({super.key});
-//
-//   @override
-//   Widget build(BuildContext context) {
-//     return StreamBuilder<User?>(
-//       stream: FirebaseAuth.instance.authStateChanges(),
-//       builder: (context, snapshot) {
-//         if (snapshot.connectionState == ConnectionState.waiting) {
-//           return const Center(child: CircularProgressIndicator());
-//         }
-//
-//         if (snapshot.hasData) {
-//           return const DashboardScreen();
-//         }
-//         return const LoginPage();
-//       },
-//     );
-//   }
-// }
